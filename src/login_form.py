c270af9d3fe367a7b207c932a892f5bd4da8927e
Erik Matos <erik_senac@hotmail.com>
2019-04-17 17:14:53 -0300

    adicionando Readme

--- src/login_form.py
+++ src/login_form.py
@@ -10,5 +10,5 @@ token = soup.find("input", {"name": "__RequestVerificationToken"})["value"]
 print(token)
 
 r = requests.post("urlpost",
-                  data={"__RequestVerificationToken": token, "Usuario": "desenv", "Password": "dev2017"})
+                  data={"__RequestVerificationToken": token, "Usuario": "user", "Password": "123"})
 print(r.cookies)
