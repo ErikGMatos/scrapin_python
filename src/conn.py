b2bb3c0bf93afa8ab48923b83cd24bec7a09ffd6
Erik Garces Matos <erik_senac@hotmail.com>
2019-04-23 17:38:56 -0300

    .

--- src/conn.py
+++ src/conn.py
@@ -7,12 +7,13 @@ cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database
 cursor = cnxn.cursor()
 
 cursor.execute("""
-select distinct  isnull(cctb.ativo,0)ativo, tb.*, isnull(cctb.percentualdesconto,0)percentualdesconto from pse.tipobolsa tb 
-                             left join pse.cursocaptacaotipobolsa cctb
-                             on tb.codtipobolsa = cctb.codtipobolsa and cctb.codcursocaptacaoperiodocaptacao = 5710
-                             left join pse.cursocaptacaoperiodocaptacao ccpc
-                             on cctb.codcursocaptacaoperiodocaptacao = ccpc.codcursocaptacaoperiodocaptacao  
-""") 
+            select distinct  isnull(cctb.ativo,0)ativo, tb.*, isnull(cctb.percentualdesconto,0)percentualdesconto from pse.tipobolsa tb 
+            left join pse.cursocaptacaotipobolsa cctb
+            on tb.codtipobolsa = cctb.codtipobolsa and cctb.codcursocaptacaoperiodocaptacao = 5710
+            left join pse.cursocaptacaoperiodocaptacao ccpc
+            on cctb.codcursocaptacaoperiodocaptacao = ccpc.codcursocaptacaoperiodocaptacao
+            """)
+             
 row = cursor.fetchone() 
 while row: 
     print(row) 
