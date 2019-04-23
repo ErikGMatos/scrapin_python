29153ef847724cff36a821c2740c190b20a89719
Erik Garces Matos <erik_senac@hotmail.com>
2019-04-23 17:13:06 -0300

    .

--- src/conn.py
+++ src/conn.py
@@ -1,12 +1,18 @@
 import pyodbc 
-server = '000.00.00.00' 
-database = 'db' 
+server = '172.16.10.50' 
+database = 'CaptacaoConversaoGraduacao' 
 username = 'sa' 
-password = '123' 
+password = 'homologacao' 
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT * from pse.cursocaptacao") 
+cursor.execute("""
+select distinct  isnull(cctb.ativo,0)ativo, tb.*, isnull(cctb.percentualdesconto,0)percentualdesconto from pse.tipobolsa tb 
+                             left join pse.cursocaptacaotipobolsa cctb
+                             on tb.codtipobolsa = cctb.codtipobolsa and cctb.codcursocaptacaoperiodocaptacao = 5710
+                             left join pse.cursocaptacaoperiodocaptacao ccpc
+                             on cctb.codcursocaptacaoperiodocaptacao = ccpc.codcursocaptacaoperiodocaptacao  
+""") 
 row = cursor.fetchone() 
 while row: 
     print(row) 
