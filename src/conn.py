39dc1071977df7907c513e19471f715c4ef452eb
Erik Garces Matos <erik_senac@hotmail.com>
2019-04-23 17:54:38 -0300

    .

--- src/conn.py
+++ src/conn.py
@@ -6,13 +6,13 @@ password = 'homologacao'
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-cursor.execute("""
+cursor.execute('''
             select distinct  isnull(cctb.ativo,0)ativo, tb.*, isnull(cctb.percentualdesconto,0)percentualdesconto from pse.tipobolsa tb 
             left join pse.cursocaptacaotipobolsa cctb
             on tb.codtipobolsa = cctb.codtipobolsa and cctb.codcursocaptacaoperiodocaptacao = 5710
             left join pse.cursocaptacaoperiodocaptacao ccpc
             on cctb.codcursocaptacaoperiodocaptacao = ccpc.codcursocaptacaoperiodocaptacao
-            """)
+            ''')
              
 row = cursor.fetchone() 
 while row: 
